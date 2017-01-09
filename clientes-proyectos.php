a1739729bf16d24a2ca8dfa6f795d5bcf5b132bc
fernando.bayley@gmail.com <fernando.bayley@gmail.com>
2017-01-08 23:33:31 -0300

    Desarrollo de paginas internas

---
+++ clientes-proyectos.php
@@ -0,0 +1,167 @@
+<?php
+/*
+Template Name: clientes-proyectos
+*/
+?>
+
+<?php get_header(); ?>
+
+
+		<section class="Clientes-proyectos">
+			<img src="<?php echo get_template_directory_uri();?>/assets/images/clientes-y-proyectos.jpg" alt="Inoxigas Clientes y pryectos">
+		</section> <!-- / Clientes-proyectos -->
+
+
+		<section class="Clientes-proyectos-caso3">
+			<h4>PROYECTO #03</h4>
+			<h2>FREDDO FOODTRUCK</h2>
+			<article>
+				<p>
+					Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+				</p>
+			</article>
+		</section> <!-- / Clientes-proyectos-caso3 -->
+
+
+		<section class="Resena">
+			<div class="Resena1">
+				<img src="<?php echo get_template_directory_uri();?>/assets/images/planos.jpg" alt="Proyectos Inoxigas Food Truck de Freddo">
+			</div> <!-- / Resena1 -->
+
+			<div class="Resena2">
+				<img src="<?php echo get_template_directory_uri();?>/assets/images/mesa-acero.jpg" alt="Inoxigas aceros inoxidables">
+
+				<article>
+					<p>
+						Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+					</p>
+				</article>
+			</div> <!-- / Resena2 -->
+		</section> <!-- / Resena -->
+
+
+		<section class="Detalles">
+			<div class="Detalles1">
+				<h2>DETALLE DEL PROYECTO</h2>
+				<article>
+					<p>
+						TIEMPOS: Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
+					</p>
+					<p>
+						MATERIALES: Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia.
+					</p>
+				</article>
+			</div> <!-- / Detalles1 -->
+
+			<div class="Detalles2">
+				<h2>CLIENTE: HELADERÍAS FREDDO</h2>
+				<article>
+					<p>
+						Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo.
+					</p>
+					<p>
+						Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+					</p>
+				</article>
+			</div> <!-- / Detalles2 -->
+		</section> <!-- / Detalles -->
+
+		
+		<!-- CLIENTES -->
+		<section class="Clientes">
+			<div class="Container">
+				<h2>CLIENTES QUE CONFÍAN EN NOSOTROS</h2>
+
+				<div class="Col1">
+					<ul>
+						<li>
+							<img src="<?php echo get_template_directory_uri();?>/assets/images/logos/bredas.jpg" alt="Bridas"images/uade-logo.png" alt="UADE">
+						</li>
+						<li>
+							<img src="<?php echo get_template_directory_uri();?>/assets/images/logos/hilton.jpg" alt="Hilton">
+						</li>
+						<li>
+							<img src="<?php echo get_template_directory_uri();?>/assets/images/logos/dashi.jpg" alt="Dashi">
+						</li>
+						<li>
+							<img src="<?php echo get_template_directory_uri();?>/assets/images/logos/lospetersen.jpg" alt="lospetersen">
+						</li>
+					</ul>
+				</div> <!-- / Clol1 -->
+
+				<div class="Col2">
+					<ul>
+						<li>
+							<img src="<?php echo get_template_directory_uri();?>/assets/images/logos/ellerstina.jpg" alt="La Ellerstina">
+						</li>
+						<li>
+							<img src="<?php echo get_template_directory_uri();?>/assets/images/logos/roux.jpg" alt="Restaurant Roux">
+						</li>
+						<li>
+							<img src="<?php echo get_template_directory_uri();?>/assets/images/logos/uade.jpg" alt="Uade">
+						</li>
+						<li>
+							<img src="<?php echo get_template_directory_uri();?>/assets/images/logos/vasalissa.jpg" alt="Vasalissa">
+						</li>
+					</ul>
+				</div> <!-- / Clol2 -->
+
+				<div class="Col3">
+					<ul>
+						<li>
+							<img src="<?php echo get_template_directory_uri();?>/assets/images/logos/lajuvenil.jpg"" alt="La Juvenil">
+						</li>
+						<li>
+							<img src="<?php echo get_template_directory_uri();?>/assets/images/logos/cimino.jpg" alt="Cimino">
+						</li>
+						<li>
+							<img src="<?php echo get_template_directory_uri();?>/assets/images/logos/freddo.jpg" alt="Freddo">
+						</li>
+						<li>
+							<img src="<?php echo get_template_directory_uri();?>/assets/images/logos/lacabrera.jpg" alt="La Cabrera">
+						</li>
+					</ul>
+				</div> <!-- / Clol3 -->
+
+				<div class="Col4">
+					<ul>
+						<li>
+							<img src="<?php echo get_template_directory_uri();?>/assets/images/logos/demuru.jpg" alt="Demuru">
+						</li>
+						<li>
+							<img src="<?php echo get_template_directory_uri();?>/assets/images/logos/melimelo.jpg" alt="Meli Melo">
+						</li>
+						<li>
+							<img src="<?php echo get_template_directory_uri();?>/assets/images/logos/donjulio.jpg" alt="Don Julio">
+						</li>
+						<li>
+							<img src="<?php echo get_template_directory_uri();?>/assets/images/logos/carrefour.jpg" alt="Ellerstina Polo">
+						</li>
+					</ul>
+				</div> <!-- / Clol4 -->
+			</div><!-- / Container -->
+		</section> <!-- / Clientes -->
+
+
+		<!-- FORMULARIO -->
+		<section class="Formulario">
+			<div class="Container">
+				<div class="Formulario-col1">
+					<h2>Háganos llegar su consulta:</h2>
+					<h3>o llamanos ahora <br>+54 11 4716-6682</h3>
+				</div> <!-- / Formulario-col1 -->
+
+				<div class="Formulario-col2">
+					<?php query_posts('category_name=form'); ?>
+						<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>		
+						<?php the_content(); ?>
+					<?php endwhile; else: ?>				
+					<?php endif; ?>
+				</div> <!-- / Formulario-col2 -->
+			</div> <!-- / Container -->
+		</section> <!-- / Formulario -->
+		<!-- / FORMULARIO -->
+
+		<section class="Franja"></section> <!-- / Franja -->
+
+<?php get_footer(); ?>
